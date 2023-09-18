b5eedb90a881281f441625364bc634e8d31d447a
Hyno03 <jenny2117@naver.com>
2023-09-18 14:13:53 +0900

    파일명 수정

--- character_squaremove.py
+++ character_squaremove.py
@@ -9,31 +9,8 @@ character = load_image('character.png')
 
 x = 0
 y = 0
-"""while(x<400):
-    clear_canvas()
-    grass.draw_now(400,30)
-    character.draw_now(400+x ,90+y)
-    x = x + 10
-    update_canvas()
-    delay(0.1)
-    if(400+x == 760):
-        while(y<500):
-            clear_canvas()
-            grass.draw_now(400,30)
-            character.draw_now(400+x ,90+y)
-            y = y + 10
-            update_canvas()
-            delay(0.1)
-            if(90+y == 400):
-                while(x == 0):
-                    clear_canvas()
-                    grass.draw_now(400,30)
-                    character.draw_now(400+x ,90+y)
-                     x = x - 10
-                    update_canvas()
-                    delay(0.1)
-"""
-for x in range(1,8):
+
+for x in range(1,7):
     clear_canvas()
     grass.draw_now(400,30)
     character.draw_now(x*100 ,90)
@@ -42,19 +19,19 @@ for x in range(1,8):
 for y in range(1,8):
      clear_canvas()
      grass.draw_now(400,30)
-     character.draw_now(800 ,y*70)
+     character.draw_now(700 ,90 + y*50)
      update_canvas()
      delay(0.1)
-for x in range(1,8):
+for x in range(1,6):
     clear_canvas()
     grass.draw_now(400,30)
-    character.draw_now(800 - x*100 ,560)
+    character.draw_now(700 - x*100 ,490)
     update_canvas()
     delay(0.1)
 for y in range(1,8):
      clear_canvas()
      grass.draw_now(400,30)
-     character.draw_now(0 ,560 -  y*70)
+     character.draw_now(100 ,490 - y*50)
      update_canvas()
      delay(0.1)    
 for x in range(1,5):
