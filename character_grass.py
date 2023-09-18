90dd459480bfb828a6f31f64b59d844c62beca7a
Hyno03 <jenny2117@naver.com>
2023-09-18 12:20:04 +0900

    Drill 2-1 제출(2022192022이예영)
    사람이 사각 운동 하는 코드 업데이트

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,69 @@
 from pico2d import *
+import math
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
 
-close_canvas()
+
+x = 0
+y = 0
+"""while(x<400):
+    clear_canvas()
+    grass.draw_now(400,30)
+    character.draw_now(400+x ,90+y)
+    x = x + 10
+    update_canvas()
+    delay(0.1)
+    if(400+x == 760):
+        while(y<500):
+            clear_canvas()
+            grass.draw_now(400,30)
+            character.draw_now(400+x ,90+y)
+            y = y + 10
+            update_canvas()
+            delay(0.1)
+            if(90+y == 400):
+                while(x == 0):
+                    clear_canvas()
+                    grass.draw_now(400,30)
+                    character.draw_now(400+x ,90+y)
+                     x = x - 10
+                    update_canvas()
+                    delay(0.1)
+"""
+for x in range(1,8):
+    clear_canvas()
+    grass.draw_now(400,30)
+    character.draw_now(x*100 ,90)
+    update_canvas()
+    delay(0.1)
+for y in range(1,8):
+     clear_canvas()
+     grass.draw_now(400,30)
+     character.draw_now(800 ,y*70)
+     update_canvas()
+     delay(0.1)
+for x in range(1,8):
+    clear_canvas()
+    grass.draw_now(400,30)
+    character.draw_now(800 - x*100 ,560)
+    update_canvas()
+    delay(0.1)
+for y in range(1,8):
+     clear_canvas()
+     grass.draw_now(400,30)
+     character.draw_now(0 ,560 -  y*70)
+     update_canvas()
+     delay(0.1)    
+for x in range(1,5):
+    clear_canvas()
+    grass.draw_now(400,30)
+    character.draw_now(x*100 ,90)
+    update_canvas()
+    delay(0.1)                    
+            
+
+
+#close_canvas()
